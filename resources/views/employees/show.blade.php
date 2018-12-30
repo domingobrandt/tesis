0e073a5e9893d7ffcdf5c579f93fe529b0343e7e
Domingo Brandt <brandtyet02@gmail.com>
2018-12-30 12:31:00 -0400

    try tu put select & name boostrap

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -13,6 +13,16 @@
          <label>Relacion</label>
          <p class="card-text">{{$employee->positions}}</p>
          <a href="/employees/{{$employee->slug}}/edit" class="btn btn-primary"> Editar </a>
+
+         <div class="box-body">
+            <div class="row">
+                {!! Form::open(['route' => 'employees.store']) !!}
+                <div class="form-group col-sm-6">
+                    {!! Form::label('posi', 'posi:') !!}
+                    {!! Form::select('posi', posi , null, ['class' => 'form-control']) !!}
+                </div>
+                {!! Form::close() !!}
+            </div>
          {!! Form::open([ 'route' => ['employees.destroy', $employee->slug], 'method' => 'DELETE']) !!}
 
     {!! Form::submit('Eliminar', ['class' => 'btn btn-danger']) !!}
