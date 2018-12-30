d2cba63e1b220a1b1094adb0275713424ebe434a
Domingo Brandt <brandtyet02@gmail.com>
2018-12-29 21:51:56 -0400

    fix view positions

--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -8,14 +8,14 @@
         <title>Prueba Tesis @yield('name')</title>
 
 
-            <p>Listado de Empleados</p>
+            <p>Listado de Cargos</p>
             @foreach($positions as $position)
             <ul>
-                <li><label>id del empleado:_</label>{{ $position->id }}</li>
-                <li><label>Nombre del empleado:_ </label>{{ $position->name }}</li>
-                <li><label>slug del empleado:_ </label>{{ $position->slug }}</li>
+                <li><label>id del Cargo:_</label>{{ $position->id }}</li>
+                <li><label>Nombre del Cargo:_ </label>{{ $position->name }}</li>
+                <li><label>slug del Cargo:_ </label>{{ $position->slug }}</li>
                     @foreach ($position->employees as $employee)
-                        <li><label>Cargo del Empleado:_</label>{{ $position->name }}</li>
+                        <li><label>Empleado del Cargo:_</label>{{ $position->name }}</li>
                     @endforeach
                     <a href="/positions/{{$position->slug}}" class="btn btn-primary">More..</a>
             </ul>
